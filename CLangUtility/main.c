e829a217ecb540cb81adfa5eb4a9177f84afbb8b
Rakshit Kathawate <rakshitjio98@gmail.com>
2025-02-15 12:12:26 +0530

    Fix the Test case with space

--- CLangUtility/main.c
+++ CLangUtility/main.c
@@ -14,7 +14,7 @@ int main(){
     // printf("%s \n", input);
 
     // char input[] = "REPEAT 4 [ FD 50 RT 90 ]";
-    char input[] = "REPEAT 1 [ FD 90 RT 90] LT 30 RT 50 REPEAT 2 [FD 30]";
+    char input[] = "REPEAT 1 [ FD 90 RT 90 ] LT 30 RT 50 REPEAT 2 [ FD 30 ]";
     char *tokens[MAX_TOKENS];
     int token_count=0;
 
@@ -66,3 +66,38 @@ int main(){
 
     return 0;
 }
+
+/**
+ * Tokens:
+Token 0: REPEAT
+Token 1: 1
+Token 2: [
+Token 3: FD
+Token 4: 90
+Token 5: RT
+Token 6: 90
+Token 7: ]
+Token 8: LT
+Token 9: 30
+Token 10: RT
+Token 11: 50
+Token 12: REPEAT
+Token 13: 2
+Token 14: [
+Token 15: FD
+Token 16: 30
+Token 17: ]
+
+ Tokens are validated 
+ Executing Command: REPEAT 1 times 
+	 Iteration 1 
+Executing Command: FD 90 
+Executing Command: RT 90 
+Executing Command: LT 30 
+Executing Command: RT 50 
+Executing Command: REPEAT 2 times 
+	 Iteration 1 
+Executing Command: FD 30 
+	 Iteration 2 
+Executing Command: FD 30 
+ */
