1f19864758dd2c9d7aa8e99df8356e2bb8c96ef6
Rakshit Kathawate <rakshitjio98@gmail.com>
2025-02-15 06:38:39 +0530

    Added Lexer for the LOGO

---
+++ PythonInterpreterWithAST/lexer.py
@@ -0,0 +1,23 @@
+
+import re
+class LogoLexer():
+
+    def __init__(self, statement):
+        self.statement = statement
+        self.pattern = "REPEAT|FD|BK|RT|LT|\d+|\]|\["
+    def tokenize(self):
+
+        parsed_tokens = re.findall(self.pattern, self.statement, re.IGNORECASE)
+        self.tokens = [ token.upper().strip() if token.isalpha() else token for token in parsed_tokens]
+        return self.tokens
+
+    def __repr__(self):
+        return "{}".format(self.tokens)
+    
+
+
+statement = "REPEAT 4 [ FD 40 RT 90 ]"
+
+obj = LogoLexer(statement)
+tokens = obj.tokenize()
+print(tokens)
