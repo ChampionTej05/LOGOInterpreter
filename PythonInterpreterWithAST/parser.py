d08072d98e37f2e58c9008436fc296ca2b2c5a0e
Rakshit Kathawate <rakshitjio98@gmail.com>
2025-02-15 06:55:39 +0530

    Added Parser, Validation & AST Creation

--- PythonInterpreterWithAST/parser.py
+++ PythonInterpreterWithAST/parser.py
@@ -6,9 +6,12 @@ class CommandNode(ASTNode):
 
     def __init__(self, operation:str, degree:int):
         super().__init__()
-        self.operations = operation
+        self.operation = operation
         # degree needs to be integer so before this should we validate the grammer and make sure everything is as per grammer
         self.degree = degree 
+
+    def __repr__(self):
+        return f"{self.operation} {self.degree}"
 
 
 class RepeatNode(ASTNode):
@@ -19,6 +22,9 @@ class RepeatNode(ASTNode):
         # List of CommandNode
         self.commands = commands 
 
+    def __repr__(self):
+        return f"REPEAT {self.repeat} {self.commands}"
+
     
 
 
@@ -26,55 +32,75 @@ class LogoParser():
 
     def __init__(self, tokens):
         self.tokens = tokens
+        self.cursor = 0 # keeps track of current token being parsed 
 
     def parse(self):
         AST = []
 
-        index = 0 
-        while index < len(self.tokens):
-            try: 
-                if self.tokens[index] in ["FD", "BK", "RT", "LT"]:
-                    # operation should be string 
-                    if not isinstance(self.tokens[index], str):
-                        raise TypeError("Expected String got ", self.tokens[index])
-                    operation = str(self.tokens[index])
-
-                    # get the degree 
-                    index+=1 
-                    #degree should be integer 
-                    if not isinstance(self.tokens[index], int):
-                        raise TypeError("Expected Integer degree, got ", self.tokens[index])
-                    degree = int(self.tokens[index])
-                    commandNode = CommandNode(operation, degree)
-                    AST.append(commandNode)
-
-                elif self.tokens[index]== 'REPEAT':
-                    
-                    #get the count 
-                    index+=1
-                    repeat = int(self.tokens[index])
-
-                    # validate the start of inner body 
-                    index+=1
-                    if self.tokens[index] != "[":
-                        raise TypeError("Expected [ for the start of the REPEAT inner body")
-
-                    #start of inner commands by skipping over [
-                    index+=1
-                    inner_command_nodes = []
-                    for i in range(repeat):
-                        commandNode = self.parse()
-                        index+=1 
-                        # how to parse inner loop without attaching tokens to the function 
-                        # we can add tokens to each function and then return AST 
-                        # or we can have single parse function and then let it run using SELF CURSOR maintained in the main object class
-                    pass
-
-                index+=1
+        while self.cursor < len(self.tokens):
+            node = self.parse_command()
+            if node :
+                AST.append(node)
+            return AST
 
 
-            except Exception as e:
-                print("Caught Parsing error ",e )
-                return e 
+    def parse_command(self):
+        try: 
+            if self.tokens[self.cursor] in ["FD", "BK", "RT", "LT"]:
+                # operation should be string 
+                if not isinstance(self.tokens[self.cursor], str):
+                    raise TypeError("Expected String got ", self.tokens[self.cursor])
+                operation = str(self.tokens[self.cursor])
+
+                # get the degree 
+                self.cursor+=1 
+                #degree should be integer 
+                if not int(self.tokens[self.cursor]):
+                    raise TypeError("Expected Integer degree, got ", self.tokens[self.cursor])
+                degree = int(self.tokens[self.cursor])
+                commandNode = CommandNode(operation, degree)
+                self.cursor+=1
+                return commandNode
+
+            elif self.tokens[self.cursor]== 'REPEAT':
+                
+                #get the count 
+                self.cursor+=1
+                repeat = int(self.tokens[self.cursor])
+
+                # validate the start of inner body 
+                self.cursor+=1
+                if self.tokens[self.cursor] != "[":
+                    raise TypeError("Expected [ for the start of the REPEAT inner body")
+
+                #start of inner commands by skipping over [
+                self.cursor+=1
+                inner_command_nodes = []
+                while self.tokens[self.cursor] != ']':
+                    # while we are not at end of stream 
+                    commandNode = self.parse_command()
+                    inner_command_nodes.append(commandNode)
+
+                self.cursor+=1
+                repeatNode = RepeatNode(inner_command_nodes, repeat)
+                return repeatNode
+            else: 
+                raise ValueError("Unexpected Token, ", self.tokens[self.cursor])
+                
+
+
+        except Exception as e:
+            print("Caught Parsing error ",e )
+            return e 
+        
+
+
+
+tokens = ['REPEAT', '4', '[', 'FD', '40', 'RT', '90', ']']
+
+obj = LogoParser(tokens)
+
+AST = obj.parse()
+print(AST)
 
 
